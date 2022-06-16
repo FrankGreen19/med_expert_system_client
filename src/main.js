27c3b1f049c104f3121b8b8b0ea618c5e6e6173e
Bulat Ahmetshin <Bulat.A.Ahmetshin@tatar.ru>
2022-06-16 21:27:20 +0300

    token to every request

--- src/main.js
+++ src/main.js
@@ -8,6 +8,11 @@ import store from './store'
 Vue.config.productionTip = false
 Vue.prototype.axios = axios
 
+const token = localStorage.getItem('token')
+if (token) {
+  Vue.prototype.axios.defaults.headers.common['Authorization'] = 'Bearer ' + token
+}
+
 new Vue({
   vuetify,
   router,
